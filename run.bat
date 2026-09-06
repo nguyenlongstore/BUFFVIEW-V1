ed74b0d04308501a032b009baba8054423be82a8
nguyenlongstore <caithuefreeben@gmail.com>
2026-09-06 18:59:34 +0700

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+title NGLONG DEV - TIKTOK VIEW BOT
+color 0F
+mode con: cols=85 lines=45
+echo ==============================================
+echo     DANG KHOI DONG TIKTOK VIEW BOT v4.0
+echo     NGLONG DEV - AUTO IP RESOLVER
+echo ==============================================
+echo.
+python bot.py
+pause
